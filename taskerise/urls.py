7404b5ec731a26641668f80ae3e17fbfc8d46cc9
Keith Richards <keith@whata.plus.com>
2024-10-16 09:51:13 +0000

    Add gitpod.yml to install requirements.txt when new workspace is created

--- taskerise/urls.py
+++ taskerise/urls.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib.auth import views as auth_views
+# 
+#from .views import TaskDetailView
+
 
 urlpatterns = [
-    
+    #
+    #path('edittask/<int:pk>/', TaskDetailView.as_view(), name='task-detail'),
+
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     # Home page path
